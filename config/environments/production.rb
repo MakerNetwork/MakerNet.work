f78548b2b102f2798a7f074c5f7afaaee03202ef
Kyle Sorus <dangersorus@Kyles-MacBook-Pro.local>
2017-04-20 00:49:05 -0700

    upgraded

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,11 @@ Rails.application.configure do
   emethod = Rails.application.secrets.delivery_method || "email"
   config.action_mailer.delivery_method = emethod
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
 end
