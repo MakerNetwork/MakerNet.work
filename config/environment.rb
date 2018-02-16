cb8894990925f2c9010e8b2e668111a8e3f9f7a2
David O' Rojo <david.orojo@gmail.com>
2018-02-16 01:42:07 -0600

    Fix configuration for email notifications

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["SMTP_USER"],
-  :password => ENV["SMTP_PASS"],
-  :domain => 'makernetwork.herokuapp.com',
-  :address => ENV["SMTP_ADDRESS"],
-  :port => ENV["SMTP_PORT"],
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
