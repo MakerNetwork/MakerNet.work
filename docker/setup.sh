bfb4ca047ee576e17ebbad509542402eab2d1341
David O' Rojo <david.orojo@gmail.com>
2018-04-21 16:31:10 -0500

    Update the links for initial configuration scripts on setup executable

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,7 +7,7 @@ prepare_config()
   mkdir -p "$MAKERNET_PATH/example"
 
   # MakerNet environment variables
-  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/0d8809730eabe6f8ee4712efabd42ef02fc9848b/env.example > "$MAKERNET_PATH/example/env.example"
+  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/f96a5dbd73e0eac51ed0c9ba29f3ad2c1162c810/env.example > "$MAKERNET_PATH/example/env.example"
 
   # nginx configuration
   \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/0d8809730eabe6f8ee4712efabd42ef02fc9848b/nginx_with_ssl.conf.example > "$MAKERNET_PATH/example/nginx_with_ssl.conf.example"
@@ -17,7 +17,7 @@ prepare_config()
   \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/0d8809730eabe6f8ee4712efabd42ef02fc9848b/webroot.ini.example > "$MAKERNET_PATH/example/webroot.ini.example"
 
   # docker-compose
-  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/8bf24055ea654fbfc43b144cc7fc2c4b5be242c7/docker-compose.yml > "$MAKERNET_PATH/docker-compose.yml"
+  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/f96a5dbd73e0eac51ed0c9ba29f3ad2c1162c810/docker-compose.yml > "$MAKERNET_PATH/docker-compose.yml"
 }
 
 prepare_config "$@"
