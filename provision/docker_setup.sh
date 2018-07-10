272d964bd98b9ed8204f382513bc88625eefa771
David O' Rojo <david.orojo@gmail.com>
2018-07-09 23:51:45 -0500

    Update provision script for the Vagrant + Docker virtual environment

--- provision/docker_setup.sh
+++ provision/docker_setup.sh
@@ -44,7 +44,7 @@ docker()
 
   echo "* Setting up Docker Compose ****************************************** "
 
-  sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 }
 
@@ -54,22 +54,7 @@ examples()
 {
   echo "* Fetching example conf files... ************************************* "
 
-  MAKERNET_PATH=${1:-/apps/makernet}
-
-  mkdir -p "$MAKERNET_PATH/example"
-
-  # MakerNet environment variables
-  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/9fdc68b0c23142ef6552e71b93728a0e7f64b518/env.example > "$MAKERNET_PATH/example/env.example"
-
-  # nginx configuration
-  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/0d8809730eabe6f8ee4712efabd42ef02fc9848b/nginx_with_ssl.conf.example > "$MAKERNET_PATH/example/nginx_with_ssl.conf.example"
-  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/0d8809730eabe6f8ee4712efabd42ef02fc9848b/nginx.conf.example > "$MAKERNET_PATH/example/nginx.conf.example"
-
-  # let's encrypt configuration
-  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/0d8809730eabe6f8ee4712efabd42ef02fc9848b/webroot.ini.example > "$MAKERNET_PATH/example/webroot.ini.example"
-
-  # docker-compose
-  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/b16c719349a6d6b12ab0f07abaf2a736f4376500/docker-compose.yml > "$MAKERNET_PATH/docker-compose.yml"
+  \curl -sSL https://gist.githubusercontent.com/MakerNetwork/1393013db25bfe9bf1ccf3dfab49cead/raw/4cc3747c972bc7e4dd7ddc252762fc15546b460f/setup.sh | sudo bash
 
   echo "* Placing basic configuration... ************************************* "
 
