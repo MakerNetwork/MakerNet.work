b71669153d4b81cdf568d3407b5ad0b3c49cb710
AnnitaCeci <anaceci.perezmorales@gmail.com>
2018-10-04 12:33:32 -0500

    remove text of signupModal view and add update gender task  in migrate file

--- db/migrate/20180928004602_change_gender_to_be_string_in_profiles.rb
+++ db/migrate/20180928004602_change_gender_to_be_string_in_profiles.rb
@@ -1,5 +1,33 @@
 class ChangeGenderToBeStringInProfiles < ActiveRecord::Migration
   def change
-    change_column :profiles, :gender, :string
+ 
+    reversible do |directive|
+      change_table :profiles do |t|
+        profiles = Profile.all
+
+        directive.up do
+          t.change :gender, :string
+
+          profiles.each do |profile|
+            case profile.gender
+            when 'true'  then profile.gender = 'man'
+            when 'false' then profile.gender = 'woman'
+            end
+            profile.save
+          end
+        end
+
+        directive.down do
+          profiles.each do |profile|
+            case profile.gender
+            when 'man' then profile.gender = true
+            else profile.gender = false
+            end
+            profile.save
+          end
+          t.change :gender, 'boolean USING CAST(gender AS boolean)'
+        end
+      end
+    end
   end
 end
