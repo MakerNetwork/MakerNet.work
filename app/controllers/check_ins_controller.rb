73cd17220b570ec5e7953a950fadf776f6399dac
Orlando Camarillo <orlandosalvadorcamarillomoreno@gmail.com>
2019-05-10 10:09:30 -0500

    corrected security vulnerability adding csrf

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,5 +1,5 @@
 class CheckInsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  #skip_before_action :verify_authenticity_token
   before_action :set_check_in, only: [:show, :edit, :update, :destroy]
 
   # GET /check_in
